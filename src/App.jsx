f51b9683e6979eace04aca9e5e6e8731034a85f2
Shalaby30 <m.a.shalaby30@gmail.com>
2024-09-03 16:47:26 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/benifits";
+import Collaboration from "./components/Collaboration";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Benefits />
+      <Collaboration />
+      <Services />
+      <ButtonGradient />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Heading.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Heading( { title , className } ) {
+  return (
+    <div className='${className} max-w[50rem} mx-auto mb-12 lg:mb-20'>{title && <h2 className="h2">{title}</h2> }</div>
+  )
+}
